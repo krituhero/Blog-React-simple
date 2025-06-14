4c4e33df31960608bf5e6ab4461582bd53baba8f
krituhero <kritarthabaniya@gmail.com>
2025-06-14 20:50:12 +0545

    some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 
 function App() {
