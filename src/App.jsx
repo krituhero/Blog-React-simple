daeb7da030aeb489f8478d1008e7992580552bad
krituhero <kritarthabaniya@gmail.com>
2025-06-14 21:02:37 +0545

    add react-router-dom to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Link, Routes,useNavigate } from 'react-router-dom'
 import './App.css'
+import Homepage from './components/Homepage'
+import Single_post from './components/Single_post'
+import Blogs from './components/Blogs'
+import Author_page from './components/Author_page'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      BLOG
-    </>
+     <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/Author" element={<Author_page />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/blog:id" element={<Single_post />} />
+    </Routes>
   )
 }
 
