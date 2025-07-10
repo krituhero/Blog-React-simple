9ca674c7983d62b19bca46fa6f65eb95d8199f2e
krituhero <kritarthabaniya@gmail.com>
2025-07-10 21:55:48 +0545

    some changes

--- src/components/Single_post.jsx
+++ src/components/Single_post.jsx
@@ -25,10 +25,7 @@ const Single_post = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.
           <br />  <span className='text-xl font-bold pt-8 block'> Plan Your Itinerary </span> <br />
           While it's essential to leave room for spontaneity and unexpected adventures, having a rough itinerary can help you make the most of your time and budget. Identify the must-see sights and experiences and prioritize them according to your interests and preferences. This will help you avoid overscheduling and ensure that you have time to relax and enjoy your journey.
-
           Vitae sapien pellentesque habitant morbi tristique. Luctus venenatis lectus magna fringilla. Nec ullamcorper sit amet risus nullam eget felis. Tincidunt arcu non sodales neque sodales ut etiam sit amet.
-
-
 
         </div>
       </div>
